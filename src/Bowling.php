378027312404474c0dfb9b738ba023791dcbd1f0
vskorkosh <v.s.korkosh@gmail.com>
2019-08-12 21:29:52 +0300

    Implements calculate method algorithm

--- src/Bowling.php
+++ src/Bowling.php
@@ -7,10 +7,83 @@ final class Bowling
 {
     public function calculate(string $scores): int
     {
-        if ($scores === 'X X X X X X X X X X X X') {
-            return 300;
+        $currentScore = 0;
+        $frameNumber = 0;
+        $wasStrike = false;
+        $twoStrikes = false;
+        $wasSpare = false;
+
+        $breakingScores = explode(' ', $scores);
+
+        foreach ($breakingScores as $frame) {
+            $frameNumber++;
+            if ($frameNumber === 11) {
+                $frame = str_replace([ 'X', '-' ], [ 10, 0 ], $frame);
+                $currentScore += $frame;
+                if ($twoStrikes) {
+                    $currentScore += $frame;
+                }
+                continue;
+            }
+            if ($frameNumber === 12) {
+                $frame = str_replace([ 'X', '-' ], [ 10, 0 ], $frame);
+                $currentScore += $frame;
+                continue;
+            }
+            $strike = $frame === 'X';
+            if ($strike) {
+                $currentScore += 10;
+                if ($twoStrikes) {
+                    $currentScore += 10;
+                }
+                if ($wasStrike) {
+                    $currentScore += 10;
+                    $twoStrikes = true;
+                }
+                if ($wasSpare) {
+                    $currentScore += 10;
+                }
+                $wasStrike = true;
+                continue;
+            }
+            $frame = str_replace('-', 0, $frame);
+            $firstFrame = $frame[ 0 ];
+            $secondFrame = $frame[ 1 ] ?? 0;
+            $spare = $secondFrame === '/';
+            if ($spare) {
+                $currentScore += 10;
+                if ($wasStrike) {
+                    $currentScore += 10;
+                }
+                if ($twoStrikes) {
+                    $currentScore += 10;
+                }
+                if ($wasSpare) {
+                    $currentScore += $firstFrame;
+                }
+                $wasSpare = true;
+                $wasStrike = false;
+                $twoStrikes = false;
+                continue;
+            }
+
+            $currentScore += $firstFrame;
+            $currentScore += $secondFrame;
+            if ($wasStrike) {
+                $currentScore += $firstFrame + $secondFrame;
+            }
+            if ($twoStrikes) {
+                $currentScore += $firstFrame;
+            }
+            if ($wasSpare) {
+                $currentScore += $firstFrame;
+            }
+            $wasSpare = false;
+            $wasStrike = false;
+            $twoStrikes = false;
+
         }
 
-        return 1;
+        return $currentScore;
     }
 }
--- src/BowlingTest.php
+++ src/BowlingTest.php
@@ -14,9 +14,45 @@ class BowlingTest extends TestCase
     }
 
     /** @test */
-    public function it_calculates_scores()
+    public function it_calculates_scores_for_10_strike_game()
+    {
+        $this->assertScoresEquals('X X X X X X X X X X - -', 270);
+    }
+
+    /** @test */
+    public function it_calculates_scores_for_9_strike_and_5_game()
+    {
+        $this->assertScoresEquals('X X X X X 5- X X X X - -', 225);
+    }
+
+    /** @test */
+    public function it_calculates_scores_for_one_bowl()
     {
         $this->assertScoresEquals('1- -- -- -- -- -- -- -- -- --', 1);
+    }
+
+    /** @test */
+    public function it_calculates_score_for_all_nine_point_frames()
+    {
+        $this->assertScoresEquals('9- 9- 9- 9- 9- 9- 9- 9- 9- 9-', 90);
+    }
+
+    /** @test */
+    public function it_calculates_score_with_spares()
+    {
+        $this->assertScoresEquals('5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5', 150);
+    }
+
+    /** @test */
+    public function it_calculates_random_score()
+    {
+        $this->assertScoresEquals('1- -- -- -- X 52 -- -- -- --', 25);
+    }
+
+    /** @test */
+    public function it_calculates_more_random_score()
+    {
+        $this->assertScoresEquals('X 5/ 5/ 5/ 52 5/ 5/ 5/ 7- 5/ 5', 141);
     }
 
     private function assertScoresEquals(string $game, int $expected)
